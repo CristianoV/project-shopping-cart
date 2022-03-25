18d13832170d16fecb1a0b50f30c025e0ea9182c
Cristiano Vieira <cristianoviieira@gmail.com>
2022-03-24 22:37:25 -0300

    primeiro requisito

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,14 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (QUERY) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`;
+
+  try {
+    const result = await fetch(url);
+    const data = await result.json();
+    const resultado = await data.results;
+    return await resultado;
+  } catch (error) {
+    console.log(`Algo deu errado :( \n${error}`);
+  }
 };
 
 if (typeof module !== 'undefined') {
--- script.js
+++ script.js
@@ -12,17 +12,33 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+  function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
+  // console.log(` não estou intendendo nada ${section}`);
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
+  // console.log(` não estou intendendo nada ${section}`);
   return section;
 }
+
+  const colocandoProduto = async () => {
+    const produto = await fetchProducts('computador');
+    const lista = produto.reduce((acc, element) => {
+      acc.sku = element.id;
+      acc.name = element.title;
+      acc.image = element.thumbnail;
+      const teste = createProductItemElement(acc);
+      const adiciona = document.querySelector('.items');
+      adiciona.appendChild(teste);
+      console.log(teste);
+      return {};    
+    }, {});
+    return lista;
+};
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
@@ -40,4 +56,6 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-window.onload = () => { };
+window.onload = () => {
+  colocandoProduto();
+ };
